e28f0b0c395f5735fe00e23cedb9e6dbd96c90ca
hexacry <hexacry@gmail.com>
2022-03-29 21:41:00 -0500

    [Add] Test_TargetID, Test_Load

--- backero_test.go
+++ backero_test.go
@@ -1,6 +1,9 @@
 package backero
 
-import "testing"
+import(
+	"testing"
+	"sync"
+)
 
 func Verify[TEG comparable](t *testing.T, expected TEG, got TEG) {
 	if expected != got {
@@ -20,4 +23,37 @@ func Test_UTI(t *testing.T) {
 	}
 
 	Verify(t, "test_v0_cp", x.String())
+}
+
+func Test_TargetID(t *testing.T) {
+	x := &Target{
+		ID: UTI{
+			Name: "tt",
+			Version: "v0",
+			Hosts: "t",
+		},
+	}
+
+	Verify(t, "tt_v0_t", x.ID.String())
+}
+
+func Test_Load(t *testing.T) {
+	x := &Cowboy{
+		sync.RWMutex{},
+		make(map[string]*Target, 0),
+	}
+
+	if err := x.Load(&Target{
+		ID: UTI{
+			Name: "test",
+			Version: "v1",
+			Hosts: "cp",
+		},
+	}); err != nil {
+		t.Error(err)
+	}
+
+	expected := "test_v1_cp"
+
+	Verify(t, expected, x.Targets[expected].ID.String())
 }
