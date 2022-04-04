d55dc5a585ef6c982ef92e3b96bb49f14e4bf806
hexacry <hexacry@gmail.com>
2022-04-03 19:26:15 -0500

    [Fixing] solving de problema :(

--- test/backero_test.cpp
+++ test/backero_test.cpp
@@ -2,13 +2,12 @@
 #include "../backero.hpp"
 
 using namespace std;
-using namespace back;
 
 string test_uti() {
-  UTI unique("test", "v0", "test");
+  back::UTI unique("test", "v0", "test");
 
   if (unique.toString() != "test_v0_test") {
-    return "test_uti: toString() is not equal to \"test_v0_test\"";
+    return "test_uti: toString() is not equal to test_v0_test";
   }
 
   return "test_uti: Done!";
